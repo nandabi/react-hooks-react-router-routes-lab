2cd722531685fbd9ade3824c82001a6a75eade5b
nandabi <valerie.nandabi@student.moringaschool.com>
2024-02-12 12:02:10 +0300

    commit message

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,30 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavBar onChangePage = {setPage}/>
+    <switch>
+      <Route path="/actors">
+       <Actors/>
+      </Route>
+
+      <Route path="/directors">
+        <Directors/>
+      </Route>
+
+      <Route path= "/movies">
+        <Movies/>
+      </Route>
+
+      <Route exact path="/">
+        <Home/>
+      </Route>
+
+      <Route path="*">
+         <h1>404 not found</h1>
+      </Route>
+    </switch>
+  </div>;
 }
 
 export default App;
